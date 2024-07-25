af8c9215c79fee8f626fcf1c100ea4ee251ba413
Miguel <miguel.ferreira15@zonmail.pt>
2024-07-25 11:44:51 +0100

    feat: add migration and setup for the join table

--- cmd/migration/migration.go
+++ cmd/migration/migration.go
@@ -1,14 +1,20 @@
 package migration
 
 import (
+	"log"
 	a "pop_culture/api/resource/Attribute"
 	m "pop_culture/api/resource/Media"
 	mt "pop_culture/api/resource/MediaType"
+	mediatypeattribute "pop_culture/api/resource/MediaTypeAttribute"
 	u "pop_culture/api/resource/User"
 
 	"gorm.io/gorm"
 )
 
 func Migrate(db *gorm.DB) {
-	db.AutoMigrate(&u.User{}, &mt.TypeMedia{}, &m.Media{}, &a.Attribute{})
+	db.AutoMigrate(&u.User{}, &mt.TypeMedia{}, &m.Media{}, &a.Attribute{}, &mediatypeattribute.TypemediaAttribute{})
+	if err := db.SetupJoinTable(&mt.TypeMedia{}, "Attributes", &mediatypeattribute.TypemediaAttribute{}); err != nil {
+		log.Fatal(err)
+		return
+	}
 }
